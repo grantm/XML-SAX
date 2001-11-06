cc74967ef1465e9dd5f4869e184a46aeda92f1d0
Matt Sergeant <matt@sergeant.org>
2001-11-06 07:59:51 +0000

    Initial implementation attempt

--- SAX.pm
+++ SAX.pm
@@ -1,51 +1,78 @@
+# $Id$
+
 package XML::SAX;
 
 use strict;
-use vars qw($VERSION @ISA @EXPORT @EXPORT_OK);
+use vars qw/$VERSION/;
+$VERSION = '0.90';
 
-require Exporter;
+sub _package_to_pm {
+    my $pk = shift;
+    $pk =~ s/::/\//g;
+    $pk .= ".pm";
+    return $pk;
+}
 
-@ISA = qw(Exporter AutoLoader);
-# Items to export into callers namespace by default. Note: do not export
-# names by default without a very good reason. Use EXPORT_OK instead.
-# Do not simply export all your public functions/methods/constants.
-@EXPORT = qw(
-	
-);
-$VERSION = '0.01';
-
-
-# Preloaded methods go here.
-
-# Autoload methods go after =cut, and are processed by the autosplit program.
+sub parser {
+    my $class = shift;
+    if ($ENV{SAX_PARSER}) {
+        require(_package_to_pm($ENV{SAX_PARSER}));
+        return $ENV{SAX_PARSER}->new(@_);
+    }
+    else {
+        require XML::SAX::PurePerl;
+        return XML::SAX::PurePerl->new(@_);
+    }
+}
 
 1;
 __END__
-# Below is the stub of documentation for your module. You better edit it!
 
 =head1 NAME
 
-XML::SAX - Perl extension for blah blah blah
+XML::SAX - Simple API for XML
 
 =head1 SYNOPSIS
 
   use XML::SAX;
-  blah blah blah
+  my $parser = XML::SAX->parser();
 
 =head1 DESCRIPTION
 
-Stub documentation for XML::SAX was created by h2xs. It looks like the
-author of the extension was negligent enough to leave the stub
-unedited.
+XML::SAX is a minimal interface to SAX parsers, that allows you to
+request a SAX parser, and be guaranteed to at least get one, 
+regardless of whether it is the best one available, or simply the
+default (XML::SAX::PurePerl).
 
-Blah blah blah.
+This distribution also contains the Perl SAX 2.0 Spec, see
+L<XML::SAX::Spec> and L<XML::SAX::AdvancedSpec>.
+
+=head1 USAGE
+
+Usage is minimal and very simple:
+
+  my $parser = XML::SAX->parser(<options>);
+
+Options are always key/value pairs, and are passed to the constructor
+of the parser object you happen to be given.
+
+To influence what kind of parser you get, set the environment variable
+SAX_PARSER to the package you'd like the parser to be from. For
+example you might prefer to use XML::SAX::Expat:
+
+  $ENV{SAX_PARSER} = 'XML::SAX::Expat';
+  my $parser = XML::SAX->parser(<options>);
+
+Please do not set this environment variable in your programs though!
+Leave that up to the user, or allow it to be set in your configuration
+files.
 
 =head1 AUTHOR
 
-A. U. Thor, a.u.thor@a.galaxy.far.far.away
+Matt Sergeant, matt@sergeant.org
 
 =head1 SEE ALSO
 
-perl(1).
+XML::SAX::Spec, XML::SAX::AdvancedSpec, XML::SAX::PurePerl
 
 =cut
