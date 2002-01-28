2f44c1bbbfec982c70ad0734733587cc591c8ed3
Matt Sergeant <matt@sergeant.org>
2002-01-28 19:35:09 +0000

    Updated Changes to 0.06 Added t/99cleanup.t to MANIFEST Added ability for load_parsers to take a directory parameter

--- SAX.pm
+++ SAX.pm
@@ -5,7 +5,7 @@ package XML::SAX;
 use strict;
 use vars qw($VERSION @ISA @EXPORT_OK);
 
-$VERSION = '0.05';
+$VERSION = '0.06';
 
 use Exporter ();
 @ISA = ('Exporter');
@@ -46,13 +46,16 @@ http://xml.org/sax/features/validation = 1
 
 sub load_parsers {
     my $class = shift;
+    my $dir = shift;
     
     # reset parsers
     $known_parsers = [];
     
     # get directory from wherever XML::SAX is installed
-    my $dir = $INC{'XML/SAX.pm'};
-    $dir = dirname($dir);
+    if (!$dir) {
+        $dir = $INC{'XML/SAX.pm'};
+        $dir = dirname($dir);
+    }
     
     my $fh = gensym();
     if (!open($fh, File::Spec->catfile($dir, "SAX", PARSER_DETAILS))) {
