83b9eb3fc2d3096ddaaa880f3dee0d5c458ab924
Grant McLean <grant@mclean.net.nz>
2011-09-18 09:38:14 +1200

    back out DebugHandler changes - not finegrained enough

--- lib/XML/SAX/PurePerl/DebugHandler.pm
+++ lib/XML/SAX/PurePerl/DebugHandler.pm
@@ -2,21 +2,11 @@ package XML::SAX::PurePerl::DebugHandler;
 
 use strict;
 use warnings;
-use Data::Dumper;
 
 sub new {
     my $class = shift;
     my %opts = @_;
     return bless \%opts, $class;
-}
-
-sub dumpthem {
-	my $self = shift;
-	if (defined($self->{seen}{_all})) {
-		$self->{seen}{_all} = $self->{seen}{_all}.Dumper(@_)."\n";
-	} else {
-		$self->{seen}{_all} = Dumper(@_)."\n";
-	}
 }
 
 # DocumentHandler
@@ -25,35 +15,30 @@ sub set_document_locator {
     my $self = shift;
     print "set_document_locator\n" if $ENV{DEBUG_XML};
     $self->{seen}{set_document_locator}++;
-	dumpthem($self,'set_document_locator: ', @_);
 }
 
 sub start_document {
     my $self = shift;
     print "start_document\n" if $ENV{DEBUG_XML};
-    $self->{seen}{start_document}++;   
-	dumpthem($self,'start_document: ', @_); 
+    $self->{seen}{start_document}++;    
 }
 
 sub end_document {
     my $self = shift;
     print "end_document\n" if $ENV{DEBUG_XML};
     $self->{seen}{end_document}++;
-	dumpthem($self,'end_document: ', @_);
 }
 
 sub start_element {
     my $self = shift;
     print "start_element\n" if $ENV{DEBUG_XML};
     $self->{seen}{start_element}++;
-	dumpthem($self,'start_element: ', @_); 
 }
 
 sub end_element {
     my $self = shift;
     print "end_element\n" if $ENV{DEBUG_XML};
     $self->{seen}{end_element}++;
-	dumpthem($self,'end_element: ', @_);
 }
 
 sub characters {
@@ -61,21 +46,18 @@ sub characters {
     print "characters\n" if $ENV{DEBUG_XML};
 #    warn "Char: ", $_[0]->{Data}, "\n";
     $self->{seen}{characters}++;
-	dumpthem($self,'characters: ', @_);
 }
 
 sub processing_instruction {
     my $self = shift;
     print "processing_instruction\n" if $ENV{DEBUG_XML};
     $self->{seen}{processing_instruction}++;
-	dumpthem($self,'processing_instruction: ', @_);
 }
 
 sub ignorable_whitespace {
     my $self = shift;
     print "ignorable_whitespace\n" if $ENV{DEBUG_XML};
     $self->{seen}{ignorable_whitespace}++;
-	dumpthem($self,'ignorable_whitespace: ', @_);
 }
 
 # LexHandler
@@ -84,7 +66,6 @@ sub comment {
     my $self = shift;
     print "comment\n" if $ENV{DEBUG_XML};
     $self->{seen}{comment}++;
-	dumpthem($self,'comment: ', @_);
 }
 
 # DTDHandler
@@ -93,14 +74,12 @@ sub notation_decl {
     my $self = shift;
     print "notation_decl\n" if $ENV{DEBUG_XML};
     $self->{seen}{notation_decl}++;
-	dumpthem($self,'notation_decl: ', @_);
 }
 
 sub unparsed_entity_decl {
     my $self = shift;
     print "unparsed_entity_decl\n" if $ENV{DEBUG_XML};
     $self->{seen}{entity_decl}++;
-	dumpthem($self,'unparsed_entity_decl: ', @_);
 }
 
 # EntityResolver
@@ -109,7 +88,6 @@ sub resolve_entity {
     my $self = shift;
     print "resolve_entity\n" if $ENV{DEBUG_XML};
     $self->{seen}{resolve_entity}++;
-	dumpthem($self,'resolve entity: ', @_);
     return '';
 }
 
